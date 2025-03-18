05ab02a75be13c5b70dbbc6183f0a15eee1241af
Pericles <slouis482@gmail.com>
2025-03-18 11:07:05 -0400

    Se setean los permisos en el menu

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -18,35 +18,32 @@ export class LayoutComponent implements OnInit {
   ngOnInit(): void {
     this.menus = menuDashboard
     this.usuario = this.loginService.getUser();
-    this.setPermission(this.usuario.permissions as unknown as Modules[] )
+    this.setPermission(this.usuario.permissions as unknown as Modules[])
   }
 
-  constructor(private loginService: LoginService, 
-    private router:Router) { }
+  constructor(private loginService: LoginService,
+    private router: Router) { }
 
-  logout(){
+  logout() {
 
     this.loginService.logout()
   }
 
-  navigate(url:string){
+  navigate(url: string) {
     this.router.navigate([url]);
   }
 
+  /**
+   * Setea los permisos de los modulos en el menu del sistema 
+   *
+   * @param {Modules[]} modules
+   * @memberof LayoutComponent
+   */
   setPermission(modules: Modules[]) {
-    const menuMap = new Map(this.menus.map(menu => [menu.id, menu]));
-  
-    modules.forEach(module => {
-      if (menuMap.has(module.id)) {
-        Object.assign(menuMap.get(module.id), {
-          view: module.view,
-          create: module.create,
-          update: module.update,
-          delete: module.delete
-        });
-      }
-    });
-  console.log(menuMap)
-  console.log(this.menus)
+    this.menus = this.menus.map(item => {
+      const perm = modules.find(p => p.id === item.id);
+      return { ...item, ...perm };
+    });
+
   }
 }
